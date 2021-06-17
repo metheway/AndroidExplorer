ef7b6323af4ccd13d607a01f90d6c9b918511ecd
黄俊萌 <552418571@qq.com>
2021-06-17 11:26:22 +0800

    用apktool版本的编译，去掉了对齐

--- build_new.sh
+++ build_new.sh
@@ -11,7 +11,7 @@ aapt2 compile --dir libs/res-design -o libs/resources-design.zip
 aapt2 compile --dir libs/res-recyclerview -o libs/resources-recyclerview.zip
 aapt2 compile --dir res -o resources.zip
 aapt2 link -I android.jar --rename-manifest-package "com.iamtrk.androidexplorer" --auto-add-overlay -R libs/resources-appcompat.zip -R libs/resources-cardview.zip -R libs/resources-design.zip -R libs/resources-recyclerview.zip --manifest AndroidManifest.xml --java gen --extra-packages android.support.v7.appcompat:android.support.v7.cardview:android.support.design:android.support.v7.recyclerview -o res.apk resources.zip
-rm -f libs/resources-appcompat.zip libs/resources-cardview.zip libs/resources-design.zip libs/resources-recyclerview.zip resources.zip
+#rm -f libs/resources-appcompat.zip libs/resources-cardview.zip libs/resources-design.zip libs/resources-recyclerview.zip resources.zip
 
 for i in libs/*.jar; do
   libs="$libs:$i"
@@ -23,20 +23,33 @@ libs="${libs#:}"
 echo " javac"
 mkdir obj
 javac -source 1.7 -target 1.7 -bootclasspath android.jar -cp $libs -sourcepath java:gen $(find java gen -type f -name '*.java') -d obj
-rm -rf gen
+#rm -rf gen
 
 echo " d8"
-d8 --release --classpath android.jar $d8libs --output . $(find obj -type f) $dxlibs
-rm -rf obj
+#echo $d8libs
+#echo $dxlibs
+#echo $(find obj -type f)
+#mkdir d8output
+mkdir d8output
+d8 --release --lib android.jar $d8libs --output d8output $(find obj -type f) $dxlibs
+#rm -rf obj
+echo "finished"
+#R文件们 $(find obj -type f), dxlibs
+# 用apktool替换apkbuilder
+echo "apktool"
+apktool d res.apk --output reverseCompiledRes
+cp ./d8output/classes.dex ./reverseCompiledRes
+apktool b reverseCompiledRes --output ./out.apk
+#echo " apkbuilder"
+#apkbuilder out.apk -u -z res.apk -f classes.dex
+#rm -f classes.dex res.apk
 
-echo " apkbuilder"
-apkbuilder out.apk -u -z res.apk -f classes.dex
-rm -f classes.dex res.apk
-
-echo " zipalign"
-zipalign 4 out.apk out-aligned.apk
-rm -f out.apk
+#echo " zipalign"
+#zipalign 4 out.apk out-aligned.apk
+#rm -f out.apk
 
 echo " apksigner"
-apksigner sign --ks debug.keystore --ks-pass pass:android --out final.apk out-aligned.apk
-rm -f out-aligned.apk
+apksigner sign --ks debug.keystore --ks-pass pass:android --out final.apk out.apk
+
+#apksigner sign --ks debug.keystore --ks-pass pass:android --out final.apk out-aligned.apk
+#rm -f out-aligned.apk
